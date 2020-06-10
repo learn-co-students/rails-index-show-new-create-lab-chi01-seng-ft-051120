0bd1e9e40cafdeb568ff8eda0e92bfb09225a30f
okaydokay97 <rubens.fernandes97@gmail.com>
2020-06-10 12:27:07 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+coupon_one = Coupon.create(coupon_code: "FREEGUAC", store: "Chipotle")
